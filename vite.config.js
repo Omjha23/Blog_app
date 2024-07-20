8f52ebdc904be699bb17d0778d4a857a3dce1531
omjha23 <omojtb2002@gmail.com>
2024-07-21 04:28:21 +0530

    vite updated

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      '/api':'http://localhost:4000/'
+      '/api':'https://blog-app-sny5.onrender.com'
     },
   },
   plugins: [react()],
